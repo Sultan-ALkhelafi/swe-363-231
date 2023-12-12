04cca5eaafe40ef3cd59c7c42cb87027d34e2cb3
sultan.dim@gmail.com <sultan@gmail.com>
2023-12-12 18:46:43 +0300

    BE 3 Ex 3

--- index.js
+++ index.js
@@ -61,3 +61,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen((3000), () => {
 	console.log("Server is Running ");
 })
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const router = require('./routes'); // Import the router
+
+app.use('/', router); // Use the router
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
